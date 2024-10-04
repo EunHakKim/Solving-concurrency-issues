920a6f65de36d1beb7a0a54675370ac3d257d946
EunHakKim <acky529@gmail.com>
2024-10-04 22:42:50 +0900

    feat: 간단한 재고 시스템 구현

---
+++ src/main/java/com/example/stock/domain/Stock.java
@@ -0,0 +1,34 @@
+package com.example.stock.domain;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.Getter;
+
+@Entity
+@Getter
+public class Stock {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    private Long productId;
+
+    private Long quantity;
+
+    public Stock() {
+    }
+
+    public Stock(Long productId, Long quantity) {
+        this.productId = productId;
+        this.quantity = quantity;
+    }
+
+    public void decrease(Long quantity) {
+        if (this.quantity < quantity) {
+            throw new RuntimeException("Not enough stock");
+        }
+        this.quantity = this.quantity - quantity;
+    }
+}
